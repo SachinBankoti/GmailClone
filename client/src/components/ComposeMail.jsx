cedbaeb04c79ddb52ed15bb0c09efe41f281330e
Sachin Bankoti <sachinbankoti16@gmail.com>
2023-09-13 12:05:47 -0400

    saveGmail from FE to DB

--- client/src/components/ComposeMail.jsx
+++ client/src/components/ComposeMail.jsx
@@ -10,6 +10,8 @@ import {
 } from "@mui/material";
 import CloseIcon from "@mui/icons-material/Close";
 import DeleteOutlineOutlinedIcon from "@mui/icons-material/DeleteOutlineOutlined";
+import useApi from "../hooks/useApi";
+import { API_URLS } from "../services/api.url";
 
 const dailogStyle = {
   height: "90%",
@@ -53,12 +55,13 @@ const SendButton = styled(Button)({
   width: 100,
 });
 const ComposeMail = ({ openDialog, setOpenDialog }) => {
-    const[data,setData] = useState({});
+  const [data, setData] = useState({});
+  const sendEmailService = useApi(API_URLS.saveSentEmail);
 
   const config = {
     Host: "smtp.elasticemail.com",
-    Username: process.env.REACT_APP_USERNAME,
-    Password: process.env.REACT_APP_PASSWORD,
+    Username: "sachingmailclone123@yopmail.com",
+    Password: "602F567B69B9F43A022151F575CB1CEDAF95",
     Port: 2525,
   };
 
@@ -77,13 +80,31 @@ const ComposeMail = ({ openDialog, setOpenDialog }) => {
         Body: data.body,
       }).then((message) => alert(message));
     }
+
+    const payload = {
+      to: data.to,
+      from: "sachinbankoti16@gmail.com",
+      subject: data.subject,
+      body: data.body,
+      date: new Date(),
+      image: "",
+      name: "Sachin Bankoti",
+      starred: false,
+      type: "sent",
+    };
+    sendEmailService.call(payload);
+    if (!sendEmailService.error) {
+      setOpenDialog(false);
+      setData({});
+    } else {
+    }
     setOpenDialog(false);
   };
 
-  const onValueChange =(e)=>{
-    setData({...data, [e.target.name]: e.target.value});
+  const onValueChange = (e) => {
+    setData({ ...data, [e.target.name]: e.target.value });
     console.log(data);
-  }
+  };
   return (
     <Dialog open={openDialog} PaperProps={{ sx: dailogStyle }}>
       <Header>
@@ -91,14 +112,22 @@ const ComposeMail = ({ openDialog, setOpenDialog }) => {
         <CloseIcon fontSize="small" onClick={(e) => closeComposeMail(e)} />
       </Header>
       <RecipientsWrapper>
-        <InputBase placeholder="Recipients" name="to" onChange={(e)=> onValueChange(e)}/>
-        <InputBase placeholder="Subject" name="subject" onChange={(e)=> onValueChange(e)}/>
+        <InputBase
+          placeholder="Recipients"
+          name="to"
+          onChange={(e) => onValueChange(e)}
+        />
+        <InputBase
+          placeholder="Subject"
+          name="subject"
+          onChange={(e) => onValueChange(e)}
+        />
       </RecipientsWrapper>
       <TextField
         multiline
         rows={20}
         sx={{ "& .MuiOutlinedInput-notchedOutlined": { border: "none" } }}
-        onChange={(e)=> onValueChange(e)}
+        onChange={(e) => onValueChange(e)}
         name="body"
       />
       <Footer>
--- server/database/db.js
+++ server/database/db.js
@@ -1,14 +1,13 @@
 import mongoose from "mongoose";
 
-
-const Connection = ()=>{
-    const DB_URL = `mongodb://user:gmailclone@ac-hos3cql-shard-00-00.ndbhblp.mongodb.net:27017,ac-hos3cql-shard-00-01.ndbhblp.mongodb.net:27017,ac-hos3cql-shard-00-02.ndbhblp.mongodb.net:27017/?ssl=true&replicaSet=atlas-tep7ll-shard-0&authSource=admin&retryWrites=true&w=majority`
-    try {
-        mongoose.connect(DB_URL, { useNewUrlParser: true});
-        console.log('Database connected successfully');
-    } catch (error) {
-        console.log('Error while connecting the database', error.message);
-    }
-}
+const Connection = () => {
+  const DB_URL = `mongodb://user:gmailclone@ac-hos3cql-shard-00-00.ndbhblp.mongodb.net:27017,ac-hos3cql-shard-00-01.ndbhblp.mongodb.net:27017,ac-hos3cql-shard-00-02.ndbhblp.mongodb.net:27017/?ssl=true&replicaSet=atlas-tep7ll-shard-0&authSource=admin&retryWrites=true&w=majority`;
+  try {
+    mongoose.connect(DB_URL, { useNewUrlParser: true });
+    console.log("Database connected successfully");
+  } catch (error) {
+    console.log("Error while connecting the database", error.message);
+  }
+};
 
 export default Connection;
